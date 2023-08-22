a4793be54bf79b3752caa773beca88f666c23bed
Sandeep <sandeep.kumar@instavans.com>
2023-08-22 13:23:18 +0530

    building

--- foodorder/src/app/home/home.component.ts
+++ foodorder/src/app/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { FoodService } from '../services/food/food.service';
 import { Foods } from 'src/app/shared/models/food'
-
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -10,7 +10,7 @@ import { Foods } from 'src/app/shared/models/food'
 })
 export class HomeComponent {
   foods:Foods[] = [];
-  constructor(private fs : FoodService){ }
+  constructor(private fs : FoodService,private router : ActivatedRoute){ }
   
   ngOnInit(){
     this.foods = this.fs.getall();
